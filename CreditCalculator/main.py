684574aa2e1609a34219ac09388cdb4c3aefd8e0
Vukain <55598879+Vukain@users.noreply.github.com>
2020-07-16 01:00:19 +0200

    fix for calc_periods
    fix for interest bigger than payments

--- CreditCalculator/main.py
+++ CreditCalculator/main.py
@@ -18,33 +18,37 @@ def arg_parser():
 
 def calc_periods():
 
-    _periods = math.ceil(math.log(monthly_payment / (monthly_payment - nominal_interest * principal), 1 + nominal_interest))
-    years = _periods // 12
-    months = _periods % 12
+    try:
+        _periods = math.ceil(math.log(monthly_payment
+                                      / (monthly_payment - nominal_interest * principal), 1 + nominal_interest))
+        years = _periods // 12
+        months = _periods % 12
 
-    plural_year = "years" if years > 1 else "year"
-    plural_month = "months" if years > 1 else "month"
-    summed_payments = _periods * monthly_payment
+        plural_year = "years" if years > 1 else "year"
+        plural_month = "months" if years > 1 else "month"
+        summed_payments = _periods * monthly_payment
 
-    if years and months:
-        print(f"You need {years} {plural_year} and {months} {plural_month} to repay this credit!")
-    elif years:
-        print(f"You need {years} {plural_year} to repay this credit!")
-    else:
-        print(f"You need {months} {plural_month} to repay this credit!")
+        if years and months:
+            print(f"You need {years} {plural_year} and {months} {plural_month} to repay this credit!")
+        elif years:
+            print(f"You need {years} {plural_year} to repay this credit!")
+        else:
+            print(f"You need {months} {plural_month} to repay this credit!")
 
-    print(f"\nOverpayment = {summed_payments - principal}")
+        print(f"\nOverpayment = {summed_payments - principal}")
+    except ValueError:
+        print("You will never repay it!!!")
+    except Exception:
+        print("I'm lost")
 
 
 def calc_annuity_payment():
 
     _monthly_payment = math.ceil(principal * ((nominal_interest * math.pow(1 + nominal_interest, periods))
-                                    / (math.pow(1 + nominal_interest, periods) - 1)))
-
+                                 / (math.pow(1 + nominal_interest, periods) - 1)))
     summed_payments = periods * _monthly_payment
 
     print(f"Your annuity payment = {math.ceil(_monthly_payment)}!")
-
     print(f"\nOverpayment = {summed_payments - principal}")
 
 
@@ -60,34 +64,33 @@ def calc_differ_payment():
         summed_payments += dif_payment
         month += 1
 
-    print(f"Overpayment = {summed_payments - principal}")
     print(f"\nOverpayment = {summed_payments - principal}")
 
 
 def calc_principal():
-    _principal = monthly_payment / ((nominal_interest * math.pow(1 + nominal_interest, periods))
-                                    / (math.pow(1 + nominal_interest, periods) - 1))
+
+    _principal = math.ceil(monthly_payment / ((nominal_interest * math.pow(1 + nominal_interest, periods))
+                                              / (math.pow(1 + nominal_interest, periods) - 1)))
     summed_payments = periods * monthly_payment
 
     print(f"Your credit principal = {_principal}!")
     print(f"\nOverpayment = {summed_payments - _principal}")
 
 
-def no_param():
-
+if len(sys.argv) == 1 or "__name__" == "__main__":
     typ = input('''
-        What do you want to calculate?
-        type "n" - for count of months,
-        type "a" - for annuity monthly payment,
-        type "d" - for differentiated payments,
-        type "p" - for credit principal: ''')
+    What do you want to calculate?
+    type "n" - for count of months,
+    type "a" - for annuity monthly payment,
+    type "d" - for differentiated payments,
+    type "p" - for credit principal: ''')
 
     interest = float(input("Enter credit interest: "))
     nominal_interest = (interest / (12 * 100))
 
     if typ == "n":
         principal = int(input("Enter credit principal: "))
-        monthly_payment = float(input("Enter monthly payment: "))
+        monthly_payment = int(input("Enter monthly payment: "))
         calc_periods()
     elif typ == "a":
         principal = int(input("Enter credit principal: "))
@@ -98,49 +101,34 @@ def no_param():
         periods = int(input("Enter count of periods: "))
         calc_differ_payment()
     elif typ == "p":
-        monthly_payment = float(input("Enter monthly payment: "))
+        monthly_payment = int(input("Enter monthly payment: "))
         periods = int(input("Enter count of periods: "))
         calc_principal()
     else:
         print("Incorrect Type")
-
-
-if len(sys.argv) == 1 and False:
-    no_param()
-
 else:
     args = arg_parser()
 
     if args.interest and len(sys.argv) == 5:
         interest = args.interest
         nominal_interest = (interest / (12 * 100))
-
         if args.type == "diff" and not args.payment:
             principal = args.principal
             periods = args.periods
             calc_differ_payment()
-
         elif args.type == "annuity" and all([args.periods, args.principal]):
             principal = args.principal
             periods = args.periods
             calc_annuity_payment()
-
         elif args.type == "annuity" and all([args.payment, args.principal]):
             principal = args.principal
             monthly_payment = args.payment
             calc_periods()
-
         elif args.type == "annuity" and all([args.payment, args.periods]):
             monthly_payment = args.payment
             periods = args.periods
             calc_principal()
-
         else:
             print("Incorrect parameters")
-
     else:
         print("Incorrect parameters")
-
-
-if "__name__" == "__main__":
-    no_param()
