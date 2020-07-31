480b171da83a6d75e7b49976cc70c0304f15e43f
Vukain <55598879+Vukain@users.noreply.github.com>
2020-07-31 22:53:13 +0200

    finished

--- SimpleBankingSystem/main.py
+++ SimpleBankingSystem/main.py
@@ -20,7 +20,7 @@ class Banking:
         Banking.cards[self.number] = self
         Banking.card_num += 1
         cur.execute(f'INSERT INTO card(id, number, pin, balance) VALUES ({Banking.card_num}, {self.number}, {self.pin}, {self.balance})')
-
+        conn.commit()
     @staticmethod
     def generator(n):
         return "".join([str(random.randint(0, 9)) for i in range(n)])
@@ -69,14 +69,61 @@ Your card PIN:
                     print("You have successfully logged in!")
                     while True:
                         print("""1. Balance
-2. Log out
+2. Add income
+3. Do transfer
+4. Close account
+5. Log out
 0. Exit
 >1""")
                         cmd = input()
                         if cmd == "1":
-                            print(f"Balance: {cls.cards[crd].balance}")
+                            blc = cur.execute(f'SELECT balance FROM card WHERE number = {crd}').fetchone()
+                            conn.commit()
+                            print(f"Balance: {blc}")
+
                         elif cmd == "2":
+                            inc = input("Enter income: \n")
+                            cur.execute(f'UPDATE card SET balance = balance + {inc} WHERE number = {crd}')
+                            conn.commit()
+                            print("Income was added!")
+
+                        elif cmd == "3":
+                            print('Transfer')
+                            print(cur.execute('SELECT number FROM card').fetchall())
+                            tran_num = input('Enter card number: \n')
+                            if tran_num == tran_num[:-1] + Banking.luhn(tran_num[:-1]):
+
+                                if (tran_num,) not in cur.execute('SELECT number FROM card').fetchall():
+                                    print('Such a card does not exist.')
+
+                                elif tran_num == crd:
+                                    print("You can't transfer money to the same account!")
+                                else:
+                                    tran_money = int(input('Enter how much money you want to transfer: \n'))
+                                    print(cur.execute(f'SELECT balance FROM card WHERE number = {crd};').fetchone()[0])
+                                    blc = cur.execute(f'SELECT balance FROM card WHERE number = {crd};').fetchone()[0]
+                                    if tran_money > int(blc):
+                                        print("Not enough money!")
+                                    else:
+                                        cur.execute(
+                                            f'UPDATE card SET balance = balance - {tran_money} WHERE number = {crd}')
+                                        cur.execute(
+                                            f'UPDATE card SET balance = balance + {tran_money} WHERE number = {tran_num}')
+                                        conn.commit()
+
+
+                            else:
+                                print('Probably you made mistake in the card number. Please try again!')
+
+                        elif cmd == "4":
+                            cur.execute(f'DELETE FROM card WHERE number = {crd}')
+                            conn.commit()
+                            print("The account has been closed!")
                             break
+
+                        elif cmd == "5":
+                            break
+
                         elif cmd == "0":
                             using = False
                             print("Bye!")
@@ -84,4 +131,3 @@ Your card PIN:
 
 
 Banking.menu()
-conn.commit()
